3b8ab7a529fa6fed6733517091a5c12ef7010d70
stevengrimes <stevengrimes83@gmail.com>
2015-08-17 13:31:08 -0700

    empty project

---
+++ my first git app 2/my first git app 2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_first_git_app_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
